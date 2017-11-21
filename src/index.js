4a50dcedbab826256eb651f71c228f0547e625a1
Emily Gordon <emgordon154@gmail.com>
2017-11-21 11:55:58 -0500

    got the map working, and markers too!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("HHHIIIIIII")
+console.log("HHHIIIIIII");
+
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiZW1nb3Jkb24xNTQiLCJhIjoiY2phOXVmNGk5MGt0eDMycXVweWEybmVrciJ9.Q310T3dOqbZ8ctXakShBWQ';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705],
+  zoom: 12,
+  style: "mapbox://styles/mapbox/streets-v10"
+});
+
+const fullstackNY = 
+  document.createElement('div');
+
+  fullstackNY.style.width = "32px";
+  fullstackNY.style.height = "39px";
+  fullstackNY.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+const fsnyMarker = new mapboxgl.Marker(fullstackNY)
+  .setLngLat([-74.009151, 40.705086])
+    .addTo(map);
+
+console.log(fsnyMarker)
+
+const fullstackChicago =
+  document.createElement('div')
+
+  fullstackChicago.style.width = "32px";
+  fullstackChicago.style.height = "39px";
+  fullstackChicago.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+
+  new mapboxgl.Marker(fullstackChicago)  
+  .setLngLat([-87.6354, 51.8885])
+  .addTo(map);
+
